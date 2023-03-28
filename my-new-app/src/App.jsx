2b085eb7c1f4a1ca488209aeeb49be177e3e6a61
SL PRODUCTION <detections.eu@gmail.com>
2023-03-28 11:57:11 +0200

    New components.1 branch setup

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <PokemonCard />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,6 +31,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
+
+    
   )
 }
 
---
+++ my-new-app/src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+    return (
+      <div>
+        <figure>
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="Bulbizarre" />
+            <figcaption>Bulbizarre</figcaption>
+        </figure>
+      </div>
+    );
+  }
+  
+  export default PokemonCard;
