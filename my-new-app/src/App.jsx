d42e443630e07abe8b71db1040c2e9da62e6de65
SL PRODUCTION <detections.eu@gmail.com>
2023-04-03 10:45:45 +0200

    React Basics 04 | Done

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+let pokemon1 = pokemonList[0];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <PokemonCard />
+      <PokemonCard name={pokemon1.name} imgSrc={pokemon1.imgSrc}/>
     </div>
 
     
--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,43 +1,29 @@
-function PokemonCard() {
+function PokemonCard(pokemon) {
 
-  let pokemon1 = pokemonList[1];
+  const { name, imgSrc } = pokemon;
 
-  if (!pokemon1.imgSrc) {
-    let test = "???";
+  if (!imgSrc) {
+    let noImg = "???";
 
     return (
       <div>
         <figure>
-          <p>{test}</p>
-          <figcaption>{pokemon1.name}</figcaption>
+          <p>{noImg}</p>
+          <figcaption>{name}</figcaption>
         </figure>
       </div>
     );
   } else {
 
-    console.log(pokemon1.imgSrc);
-
     return (
       <div>
         <figure>
-            <img src={pokemon1.imgSrc} alt={pokemon1.name} />
-            <figcaption>{pokemon1.name}</figcaption>
+            <img src={imgSrc} alt={name} />
+            <figcaption>{name}</figcaption>
         </figure>
       </div>
     );
   }
 }
-
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
